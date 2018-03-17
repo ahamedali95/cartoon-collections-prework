05bab53f6de980b1956986567b07d2d95055deed
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-17 19:06:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,11 @@ def long_planeteer_calls(planeteer_calls)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(items)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  items.find do |item|
+    cheese_types.include?(item)
+  end
 end
