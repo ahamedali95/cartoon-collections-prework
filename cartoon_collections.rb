ec93c8967fda2c5ea4c337c0b3d8e3f638858b0f
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-17 18:08:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  idx = 0
+  while idx < dwarf_names
+    puts "#{idx + 1} #{dwarf_names[i]}"
+  end
 end
 
 def summon_captain_planet# code an argument here
